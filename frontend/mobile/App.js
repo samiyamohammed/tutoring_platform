80707ac559bc0fbf99c40c81a1cb3f17a0b57866
Iman Mohammedsaeed <imanahmedmohs@gmail.com>
2025-03-13 14:27:23 +0300

    updated

--- frontend/mobile/App.js
+++ frontend/mobile/App.js
@@ -4,6 +4,7 @@ import {
   createStackNavigator,
   CardStyleInterpolators,
 } from "@react-navigation/stack";
+
 import SignUpScreen from "./screens/SignUpScreen";
 import SignInScreen from "./screens/SignInScreen";
 import ResetPasswordScreen from "./screens/ResetPasswordScreen";
@@ -11,29 +12,38 @@ import OnboardingScreen1 from "./screens/OnboardingScreen1";
 import OnboardingScreen2 from "./screens/OnboardingScreen2";
 import OnboardingScreen3 from "./screens/OnboardingScreen3";
 
+import BottomTabs from "./components/BottomNav";
+
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        screenOptions={({ route, navigation }) => ({
+        initialRouteName="Main"
+        screenOptions={({ navigation }) => ({
           headerShown: false,
           gestureEnabled: true,
           gestureDirection: navigation.canGoBack()
             ? "horizontal-inverted"
-            : "horizontal", // Fix direction for back swipe
+            : "horizontal",
           cardStyleInterpolator: navigation.canGoBack()
-            ? CardStyleInterpolators.forFadeFromBottomAndroid // Left-to-right for back
-            : CardStyleInterpolators.forHorizontalIOS, // Right-to-left for forward
+            ? CardStyleInterpolators.forFadeFromBottomAndroid
+            : CardStyleInterpolators.forHorizontalIOS,
         })}
       >
+        {/* Onboarding Screens */}
         <Stack.Screen name="Onboarding" component={OnboardingScreen1} />
         <Stack.Screen name="Onboarding2" component={OnboardingScreen2} />
         <Stack.Screen name="Onboarding3" component={OnboardingScreen3} />
+
+        {/* Authentication Screens */}
         <Stack.Screen name="SignUp" component={SignUpScreen} />
         <Stack.Screen name="SignIn" component={SignInScreen} />
         <Stack.Screen name="ResetPassword" component={ResetPasswordScreen} />
+
+        {/* Main App - Bottom Navigation */}
+        <Stack.Screen name="Main" component={BottomTabs} />
       </Stack.Navigator>
     </NavigationContainer>
   );
