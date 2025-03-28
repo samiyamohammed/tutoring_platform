7de5a76083cdd4718beb75582f72f8701484b42d
Michael Engida <mikiengida52@gmail.com>
2025-03-28 21:37:19 +0300

    feat: shadcn installation and login ui refactor

--- frontend/web/vite.config.js
+++ frontend/web/vite.config.js
@@ -1,6 +1,8 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +10,11 @@ export default defineConfig({
     react(),
     tailwindcss(),
     ],
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
 
