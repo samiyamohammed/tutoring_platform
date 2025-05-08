67fdbc380c29b0fc01e7cb50b36804791e9041d4
shilimat <119524195+shilimat@users.noreply.github.com>
2025-05-08 07:28:26 +0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,7 +31,7 @@ app.use(express.static('uploads')); // Serve static files from the uploads direc
 
 // Update your CORS configuration
 const corsOptions = {
-    origin: ['http://127.0.0.1:5500', 'http://localhost:5500', 'http://localhost:3000'],
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5500', 'http://localhost:3000', 'https://learning-management-g043.onrender.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
