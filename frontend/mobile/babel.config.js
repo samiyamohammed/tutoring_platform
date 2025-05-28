6d12f779fd7c80c34edee9c666c0c5074f22dbd6
Iman Mohammedsaeed <imanahmedmohs@gmail.com>
2025-05-28 23:07:39 +0300

    The finalised code

--- frontend/mobile/babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      ['module:react-native-dotenv', {
-        moduleName: '@env',
-        path: '.env', // Path to your .env file
-      }],
-    ],
-  };
--- frontend/mobile/src/utils/validation.js
+++
@@ -1,9 +0,0 @@
-export const validateEmail = (email) => {
-    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(email);
-  };
-  
-  export const validatePassword = (password) => {
-    return password.length >= 6;
-  };
-  
